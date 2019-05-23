fb91dc8288f1a4913527bc92f46944640a324c64
Eungu-Kwon <kiwi9084@gmail.com>
2019-05-23 17:40:03 +0900

    (5/22) init

---
+++ app/src/main/java/com/eungu/habittraining/DBHelper.java
@@ -0,0 +1,26 @@
+package com.eungu.habittraining;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+public class DBHelper extends SQLiteOpenHelper {
+    Context c;
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        c = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE training (_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, done INTEGER)");
+        Toast.makeText(c.getApplicationContext(), "Make Table", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS training");
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/eungu/habittraining/MainActivity.java
+++ app/src/main/java/com/eungu/habittraining/MainActivity.java
@@ -1,13 +1,44 @@
 package com.eungu.habittraining;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private DBHelper m_helper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        m_helper = new DBHelper(getApplicationContext(), "training.db", null, 1);
+
+        Button b1 = findViewById(R.id.button1);
+        Button b2 = findViewById(R.id.button2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db = m_helper.getWritableDatabase();
+                db.execSQL("INSERT INTO training VALUES (NULL, 'whattodo', 0)");
+                Toast.makeText(MainActivity.this, "Add Record", Toast.LENGTH_LONG).show();
+                db.close();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db = m_helper.getWritableDatabase();
+                db.execSQL("DROP TABLE training");
+                db.close();
+                Toast.makeText(MainActivity.this, "Delete Table", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
